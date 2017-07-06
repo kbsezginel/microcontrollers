81bb4523e7d5ef42016fc796e351f109d07cd928
kbsezginel <kbs37@pitt.edu>
2017-07-06 18:39:54 -0400

    Initial files for Flask web server

---
+++ FlaskApp/main.py
@@ -0,0 +1,35 @@
+import datetime
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'name': name,
+        'time': timeString
+    }
+    return render_template('hello.html', **templateData)
+
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('/'))
+    return render_template('login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
