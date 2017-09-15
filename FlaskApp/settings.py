7fb6ad9bedbcbbebf74ad4ff5931e3f906336575
kbsezginel <kbs37@pitt.edu>
2017-09-14 23:01:45 -0400

    Update web server settings.

--- FlaskApp/settings.py
+++ FlaskApp/settings.py
@@ -1,21 +1,25 @@
 """
 Web server settings
 """
-# Enter codes for each outlet
+# 433 MHz Outlet Codes -----------------------------------------------------------------------------
 rfcodes = {'1': {'on': '21811', 'off': '21820'},
            '2': {'on': '21955', 'off': '21964'},
            '3': {'on': '22275', 'off': '22284'},
            '4': {'on': '23811', 'off': '23820'},
            '5': {'on': '29955', 'off': '29964'}}
-
-
-led_pins = {'on': 24, 'off': 23, 'num': 3, 'speed': 0.15}
+# 433 MHz transmission settings --------------------------------------------------------------------
 codesend = {'pin': '0',               # Set pin number (GPIO: 17)
             'length': '189',          # Set pulse length
             'exec': './codesend'}     # Set codesend script path (should be in rfoutlet)
-
-
+# LED settings -------------------------------------------------------------------------------------
+led_settings = {'on': 24,             # Green LED GPIO
+                'off': 23,            # Red LED GPIO
+                'num': 2,             # Number of times to blink
+                'speed': 0.15,        # Speed of blink (s)
+                'blink': False}       # Blink when outlets are switched
+# Bus API settings ---------------------------------------------------------------------------------
 pgh_api_key = "LMJrK9vutafSVnViFmFvjaXvY"
 bus_stops = {'id': [8245, 3144, 8192],
              'name': ['Summerlea', 'Bellefonte', 'Graham'],
              'max_prd': 1}
+# --------------------------------------------------------------------------------------------------
--- FlaskApp/settings_sample.py
+++ FlaskApp/settings_sample.py
@@ -1,20 +1,25 @@
 """
 Web server settings
 """
-# Enter codes for each outlet
+# 433 MHz Outlet Codes -----------------------------------------------------------------------------
 rfcodes = {'1': {'on': '', 'off': ''},
            '2': {'on': '', 'off': ''},
            '3': {'on': '', 'off': ''},
            '4': {'on': '', 'off': ''},
            '5': {'on': '', 'off': ''}}
-
-
-led_pins = {'on': 24, 'off': 23, 'num': 3, 'speed': 0.15}
+# 433 MHz transmission settings --------------------------------------------------------------------
 codesend = {'pin': '0',               # Set pin number (GPIO: 17)
             'length': '189',          # Set pulse length
             'exec': './codesend'}     # Set codesend script path (should be in rfoutlet)
-
+# LED settings -------------------------------------------------------------------------------------
+led_settings = {'on': 24,             # Green LED GPIO
+                'off': 23,            # Red LED GPIO
+                'num': 2,             # Number of times to blink
+                'speed': 0.15,        # Speed of blink (s)
+                'blink': True}        # Blink when outlets are switched
+# Bus API settings ---------------------------------------------------------------------------------
 pgh_api_key = ''                      # PGH API key
 bus_stops = {'id': [],                # PGH bus stop id
              'name': [],              # Bus stop name
              'max_prd': 1}            # Max number of predictions
+# --------------------------------------------------------------------------------------------------
